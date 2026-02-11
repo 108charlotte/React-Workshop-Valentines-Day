9d8b1621faa4d79405e31a61d6fb9f0fd3451d9c
lolasanchezz <lolansanchez@icloud.com>
2026-02-11 13:03:54 -0500

    added sticker, added map function, added a little css and some background'

--- react-workshop/src/App.tsx
+++ react-workshop/src/App.tsx
@@ -1,18 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import Card from './Card.tsx'
-import Hearts from './Hearts.tsx'
-import XOXO from './XOXO.tsx'
+
+import Sticker from './Sticker.tsx'
 import './App.css'
 
 function App() {
+
+  const imageSrcs = [
+'https://i.pinimg.com/originals/1b/58/31/1b5831dbd8900841d372d9b66a62156a.gif',
+'https://i.pinimg.com/originals/4d/b8/3e/4db83e362eb13a0f7c29513986c70603.gif',
+'https://freepngimg.com/save/22279-cute-cat/828x957',
+'https://static.vecteezy.com/system/resources/thumbnails/035/149/157/small/ai-generated-cute-kitty-cat-with-christmas-hat-no-background-perfect-for-print-on-demand-merchandise-png.png',
+'https://png.pngtree.com/png-clipart/20250825/original/pngtree-hyper-realistic-wet-red-lips-with-tongue-out-on-white-background-png-image_21387485.png',
+'https://png.pngtree.com/png-clipart/20240913/original/pngtree-grimacing-face-emoji-realistic-3d-on-transparent-png-image_16002009.png'
+]
+
+  const sizes = [
+    50, 300, 100, 100, 20
+  ]
+
+
   return (
     <>
       <Card/>
-      <Hearts/>
-      <XOXO/>
-      <Hearts/>
+      {imageSrcs.map((image, i) => (
+        <Sticker key={image} imageSrc={image} size = {sizes[i]}/>
+      ))}
+     
     </>
   )
 }
---
+++ react-workshop/src/Sticker.tsx
@@ -0,0 +1,61 @@
+import {useState, useEffect, useRef} from 'react'
+
+function Sticker(props: {imageSrc: string, intX?: number, intY?: number, size?: number}) {
+
+    const intX = props.intX ?? Math.random() * (window.innerWidth/2);
+    const intY = props.intY ?? Math.random() * (window.innerHeight/2);
+ 
+    const [position, setPosition] = useState({x: intX, y: intY})
+    const [isDragging, setIsDragging] = useState(false)
+    const offset = useRef({x: 0, y: 0})
+
+    const handleMouseDown = (e) => {
+       //makes the image dragging less buggy
+        e.preventDefault() 
+
+        offset.current = {
+            x: e.clientX - position.x,
+            y: e.clientY - position.y
+        }
+        setIsDragging(true)
+    }
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            if (isDragging) {
+                setPosition({
+                    x: e.clientX - offset.current.x,
+                    y: e.clientY - offset.current.y
+                })
+            }
+        }
+
+        const handleMouseUp = () => {
+            setIsDragging(false)
+        }
+
+        if (isDragging) {
+            window.addEventListener('mousemove', handleMouseMove)
+            window.addEventListener('mouseup', handleMouseUp)
+            
+            return () => {
+                window.removeEventListener('mousemove', handleMouseMove)
+                window.removeEventListener('mouseup', handleMouseUp)
+            }
+        }
+    }, [isDragging])
+
+    return (
+        <img src={props.imageSrc} onMouseDown={handleMouseDown}
+        style={{
+            position: 'absolute',
+            left: position.x + 'px', 
+            top: position.y + 'px', 
+            cursor: 'grab', 
+            zIndex: 2, 
+            width: props.size ? props.size + 'px' : '100px'
+        }}/>
+    )
+}
+
+export default Sticker
