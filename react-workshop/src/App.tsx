12920d92dae8ddbb3c36e2b40ee9dc0224270f90
lolasanchezz <lolansanchez@icloud.com>
2026-02-11 13:49:13 -0500

    added more images

--- react-workshop/src/App.tsx
+++ react-workshop/src/App.tsx
@@ -10,6 +10,8 @@ function App() {
 'https://i.pinimg.com/originals/4d/b8/3e/4db83e362eb13a0f7c29513986c70603.gif',
 'https://freepngimg.com/save/22279-cute-cat/828x957',
 'https://static.vecteezy.com/system/resources/thumbnails/035/149/157/small/ai-generated-cute-kitty-cat-with-christmas-hat-no-background-perfect-for-print-on-demand-merchandise-png.png',
+'https://mystickermania.com/cdn/stickers/158-512x512.png',
+'https://raw.githubusercontent.com/108charlotte/React-Workshop-Valentines-Day/24b18d5303cf47d6caf263820a80cefc3321d97b/react-workshop/src/assets/hearts.png'
 ]
 
   const sizes = [
